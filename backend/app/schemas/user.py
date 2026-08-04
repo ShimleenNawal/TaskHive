5721553a4b6beb6ba3aa94e05788ccf8902ed24d
Shimleen <nshimleen@gmail.com>
2026-08-04 09:30:39 +0600

    feat(auth): create pydantic schemas for auth endpoints

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, EmailStr, Field, field_validator  
+
+class UserCreate(BaseModel): # Sign-up
+    name: str
+    email: EmailStr # Email format validation
+    password: str = Field(..., min_length = 8) # min 8 chars
+
+    @field_validator('password') # custom pydantic password validation 
+    @classmethod
+    def validate_password(cls, v):
+        # Check for at least 1 letter
+        if not any(c.isalpha() for c in v):
+            raise ValueError('Password must contain at least 1 letter')
+        
+        # Check for at least 1 digit
+        if not any(c.isdigit() for c in v):
+            raise ValueError('Password must contain at least 1 digit')
+        
+        return v
+
+class UserOut(BaseModel):
+    id: int
+    name: str
+    email: str # email already validated 
+    is_verified: bool
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class LoginResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer" # default for JWT 
