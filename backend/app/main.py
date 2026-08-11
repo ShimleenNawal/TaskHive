e1ee7aab88c7ef473a2b8f0656cc1c772111d35c
Shimleen <nshimleen@gmail.com>
2026-08-11 10:41:27 +0600

    fix(port): add changed port to CORS and Docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from app.routers import auth, users 
+from app.routers import auth, user 
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,4 +17,4 @@ def get_status():
     return {"status": "ok"}
 
 app.include_router(auth.router, prefix = "/api")
-app.include_router(users.router, prefix="/api")
+app.include_router(user.router, prefix="/api")
